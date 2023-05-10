f0bc26f889fb31af02181e23a21f4f675f6f2fcc
reotsugua <renanaugusto.ras@gmail.com>
2023-05-10 20:43:48 -0300

    Assumptions e Testes condicionais

--- testeMaven/src/test/java/org/example/CondicionaisTeste.java
+++ testeMaven/src/test/java/org/example/CondicionaisTeste.java
@@ -1,4 +1,18 @@
 package org.example;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assumptions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.*;
+
 public class CondicionaisTeste {
+
+    @Test
+    //@EnabledForJreRange(min = JRE.JAVA_8, max = JRE.JAVA_16)
+    @EnabledOnOs({OS.LINUX, OS.WINDOWS})
+    @EnabledOnJre(JRE.JAVA_17)
+    //@EnabledIfEnvironmentVariable(named = "USER", matches = "Renan")
+    void validarAlgoSomenteNoUsuarioWillyan(){
+        Assertions.assertEquals(10, 5+5);
+    }
 }
