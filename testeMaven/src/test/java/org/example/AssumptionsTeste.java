c5935cb7ff60ca0e65e02e02b0d5789cd6965906
reotsugua <renanaugusto.ras@gmail.com>
2023-05-10 20:28:41 -0300

    Assumptions e Testes condicionais

--- testeMaven/src/test/java/org/example/AssumptionsTeste.java
+++ testeMaven/src/test/java/org/example/AssumptionsTeste.java
@@ -1,5 +1,15 @@
 package org.example;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assumptions;
+import org.junit.jupiter.api.Test;
+
 public class AssumptionsTeste {
+
+    @Test
+    void validarAlgoSomenteNoUsuarioWillyan(){
+        Assumptions.assumeTrue("Renan".equals("Renan"));
+        Assertions.assertEquals(10, 5+5);
+    }
 
 }
