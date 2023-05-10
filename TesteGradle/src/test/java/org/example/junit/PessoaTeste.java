81296a92f70a0cb25dcb1b0515a81f54dfb78bc1
reotsugua <renanaugusto.ras@gmail.com>
2023-05-10 17:28:01 -0300

    JUnit no Gradle

--- TesteGradle/src/test/java/org/example/junit/PessoaTeste.java
+++ TesteGradle/src/test/java/org/example/junit/PessoaTeste.java
@@ -1,4 +1,16 @@
 package org.example.junit;
 
+import org.example.Pessoa;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
 public class PessoaTeste {
+
+    @Test
+    void validarCalculoIdade(){
+        Pessoa pessoa = new Pessoa("Julia", LocalDate.of(2021, 01, 01));
+        Assertions.assertEquals(2, pessoa.getIdade());
+    }
 }
