2dd5bd4a311f5b17b150b1b7dc73c4de9a7297b4
reotsugua <renanaugusto.ras@gmail.com>
2023-05-10 17:22:55 -0300

    JUnit no Gradle

--- TesteGradle/src/main/java/org/example/Main.java
+++ TesteGradle/src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Pessoa humano = new Pessoa("Renan",LocalDate.of(1992,03,05));
+
+        System.out.println(humano.getIdade());
+
     }
 }
---
+++ TesteGradle/src/test/java/org/example/junit/PessoaTeste.java
@@ -0,0 +1,4 @@
+package org.example.junit;
+
+public class PessoaTeste {
+}
