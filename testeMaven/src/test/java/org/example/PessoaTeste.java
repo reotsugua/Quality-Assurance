cf329edae79da942489ba4471fe940f5c3098660
reotsugua <renanaugusto.ras@gmail.com>
2023-05-10 17:56:10 -0300

    JUnit com Maven

--- testeMaven/src/test/java/org/example/PessoaTeste.java
+++ testeMaven/src/test/java/org/example/PessoaTeste.java
@@ -1,5 +1,24 @@
 package org.example;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
 public class PessoaTeste {
+        Pessoa pessoa = new Pessoa(
+                "Jéssica",
+                LocalDateTime.of(2000,1,1,15,0,0)
+        );
+
+    @Test
+    void deveCalcularIdadeCorretamente(){
+        Assertions.assertEquals(23, pessoa.getIdade());
+    }
+
+    @Test
+    void deveRetornarSeEhMaiorDeIdade(){
+        Assertions.assertTrue(pessoa.ehMaiorDeIdade());
+    }
 
 }
