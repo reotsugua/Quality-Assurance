5dcde86ec86b43869fb3311f8d7e67827fc651ba
reotsugua <renanaugusto.ras@gmail.com>
2023-05-10 20:53:32 -0300

    Testando exceptions

--- testeMaven/src/test/java/org/example/ExceptionsTeste.java
+++ testeMaven/src/test/java/org/example/ExceptionsTeste.java
@@ -1,4 +1,21 @@
 package org.example;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class ExceptionsTeste {
+
+    @Test
+    void validarCenarioDeExcecaoNaTransferencia(){
+        Conta contaOrigem = new Conta("123456", 0);
+        Conta contaDestino = new Conta("654321", 100);
+
+        TransferenciaEntreContas transferenciaEntreContas = new TransferenciaEntreContas();
+
+        Assertions.assertThrows(IllegalArgumentException.class, () ->
+                transferenciaEntreContas.transfere(contaOrigem,contaDestino,-1));
+
+        Assertions.assertDoesNotThrow(() ->transferenciaEntreContas.transfere(contaOrigem,contaDestino,10));
+
+    }
 }
