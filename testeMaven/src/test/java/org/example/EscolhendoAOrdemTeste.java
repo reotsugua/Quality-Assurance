6b692b5e2f35ac49d9a8065e0398da83c63861a7
reotsugua <renanaugusto.ras@gmail.com>
2023-05-10 21:09:47 -0300

    Ordenando testes

--- testeMaven/src/test/java/org/example/EscolhendoAOrdemTeste.java
+++ testeMaven/src/test/java/org/example/EscolhendoAOrdemTeste.java
@@ -1,4 +1,34 @@
 package org.example;
 
+import org.junit.jupiter.api.*;
+
+//@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+//@TestMethodOrder(MethodOrderer.MethodName.class) // escolhe ordem dos nome
+//@TestMethodOrder(MethodOrderer.Random.class) // randomico
+@TestMethodOrder(MethodOrderer.DisplayName.class) // ordem do Display name
 public class EscolhendoAOrdemTeste {
+    //@Order(3)
+
+    @DisplayName("B")
+    @Test
+    void validaFluxoA() {
+        Assertions.assertTrue(true);
+    }
+    //@Order(1)
+    @DisplayName("A - Teste que testa tal coisa")
+    @Test
+    void validaFluxoB() {
+        Assertions.assertTrue(true);
+    }
+    //@Order(2)
+    @DisplayName("01")
+    @Test
+    void validaFluxoC() {
+        Assertions.assertTrue(true);
+    }
+    @DisplayName("02")
+    @Test
+    void validaFluxoD() {
+        Assertions.assertTrue(true);
+    }
 }
