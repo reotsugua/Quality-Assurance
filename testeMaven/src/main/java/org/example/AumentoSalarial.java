86a870a856220034b3b4ae1e59e3f46521710bfe
reotsugua <renanaugusto.ras@gmail.com>
2023-05-12 19:46:51 -0300

    Atividade da faculdade

--- testeMaven/src/main/java/org/example/AumentoSalarial.java
+++ testeMaven/src/main/java/org/example/AumentoSalarial.java
@@ -1,9 +1,11 @@
+package org.example;
+
 import java.util.Scanner;
 
 public class AumentoSalarial {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        double salarioMinimo = 1100.0; // valor do salário mínimo atual em reais
+        double salarioMinimo = 1320.0;
         double aumentoPercentual;
 
         System.out.print("Digite o percentual de aumento do salário: ");
@@ -12,7 +14,7 @@ public class AumentoSalarial {
         double aumento = salarioMinimo * aumentoPercentual / 100.0;
         double salarioReajustado = salarioMinimo + aumento;
 
-        System.out.printf("Salário: R$ %.2f%n", salarioMinimo);
+        System.out.printf("Salário mínimo: R$ %.2f%n", salarioMinimo);
         System.out.printf("Salário reajustado com aumento de %.2f%%: R$ %.2f%n", aumentoPercentual, salarioReajustado);
 
         scanner.close();
